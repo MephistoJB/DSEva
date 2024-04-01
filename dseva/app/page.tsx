30338f1a98b99476349eda758e99a455067aa3ac
MephistoJB <bjpb@gmx.de>
2024-04-01 22:58:53 +0200

    Training Results

---
+++ dseva/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <h2 className="text-empha">Header</h2>
+      Text für DSEva
+    </main>
+  );
+}
---
+++ installDependencies.sh
@@ -0,0 +1,7 @@
+apt update
+apt install wget
+apt install npm -y
+npm install -g n
+n latest
+n prune
+rm -rf /var/lib/apt/lists/*
